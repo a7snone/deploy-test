33691d690587e825fc78cfa2ac2e59aa5b729bf5
System Administrator <root@Fahads-MacBook-Pro.local>
2020-03-03 12:29:55 +0300

    Updates

---
+++ precache-manifest.222ebc5a9354fd24ab49a99f0f4e9a65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f9c9ff4a3dfe1f28256313b52da571b",
+    "url": "/deploy-test/index.html"
+  },
+  {
+    "revision": "f61164920d999d1e454d",
+    "url": "/deploy-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fe46650c3fc650dacfe7",
+    "url": "/deploy-test/static/js/2.e0e1c986.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/deploy-test/static/js/2.e0e1c986.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f61164920d999d1e454d",
+    "url": "/deploy-test/static/js/main.4de13567.chunk.js"
+  },
+  {
+    "revision": "9487d299211ab210f28f",
+    "url": "/deploy-test/static/js/runtime-main.9d267c26.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
